3f8271ac9d5ea446dfd893cb584cacd6c1c84a7a
Yulia Sukhar <yulia-s@i.ua>
2020-12-02 15:16:48 +0200

    4 задача з виправленнями

--- js/task-4.js
+++ js/task-4.js
@@ -3,15 +3,18 @@ const pricePerDroid = 3000;
 let message;
 
 let quantity = prompt('Введіть кількість');
-let totalPrice = pricePerDroid * parseInt(quantity);
 if (quantity === null) {
   message = 'Скасовано користувачем!';
   alert(message);
-} else if (totalPrice > credits) {
-  message = 'Недостатньо коштів на рахунку!';
-  alert(message);
-} else if (totalPrice > 0 && totalPrice <= credits) {
-  credits = credits - totalPrice;
-  message = `Ви купили ${quantity} дроїдів, на рахунку залишилося ${credits} кредитів.`;
-  alert(message);
+} else if (quantity !== null) {
+  let totalPrice = pricePerDroid * parseInt(quantity);
+
+  if (totalPrice > credits) {
+    message = 'Недостатньо коштів на рахунку!';
+    alert(message);
+  } else if (totalPrice > 0 && totalPrice <= credits) {
+    credits = credits - totalPrice;
+    message = `Ви купили ${quantity} дроїдів, на рахунку залишилося ${credits} кредитів.`;
+    alert(message);
+  }
 }
